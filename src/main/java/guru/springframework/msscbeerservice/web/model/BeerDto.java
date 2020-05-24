7be67d62ae6ec18f3b30e0284eeab043dd5eed4f
Alberto <alberto.poyatos.uxo@gmail.com>
2020-05-24 18:19:30 +0200

    Add Bean validation

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerDto.java
@@ -8,20 +8,31 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BeerDto {
+    @Null
     private UUID id;
+    @Null
     private Integer version;
-
+    @Null
     private OffsetDateTime createdDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
 
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @NotNull
+    @Positive
     private Long upc;
     private BigDecimal price;
     private Integer quantityOnHand;
